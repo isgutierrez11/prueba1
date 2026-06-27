ef2177cb5772ef1ea66daea515ce8170fd0205b1
Bryan <bryanlaraarguello@gmail.com>
2026-06-27 15:23:42 -0600

    Creacion de clase abstracta persona
    Creacion de los atributos protegidos identificacion,nombrecompleto,fechadenacimiento,telefono con sus metodos get,set y respectivo constructor
    
    correccion

--- src/personas/Persona.java
+++ src/personas/Persona.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package personas;
 
-/**
- *
- * @author bryan
- */
-public class Persona {
+package personas;
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Persona {
+    
+    protected int identificacion;
+    protected String nombrecompleto;
+    protected LocalDate fechadenacimiento;
+    protected int telefono;
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public String getNombrecompleto() {
+        return nombrecompleto;
+    }
+
+    public LocalDate getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+    
+    public int getEdad(){
+        return Period.between(fechadenacimiento, LocalDate.now()).getYears();
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona(int identificacion, String nombrecompleto, LocalDate fechadenacimiento, int telefono) {
+        this.identificacion = identificacion;
+        this.nombrecompleto = nombrecompleto;
+        this.fechadenacimiento = fechadenacimiento;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "identificacion=" 
+                + identificacion + ", nombrecompleto=" 
+                + nombrecompleto + ", fechadenacimiento=" 
+                + fechadenacimiento + ", telefono=" 
+                + telefono + '}';
+    }
+    
     
 }
