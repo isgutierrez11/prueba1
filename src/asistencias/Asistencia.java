758df0a6f40327653675ac4e4a431351eefe7ff2
Bryan <bryanlaraarguello@gmail.com>
2026-06-27 16:07:11 -0600

    Creacion de la clase Asistencia
    Con boolean para la asistencia en entrenamiento y sus get y set

--- src/asistencias/Asistencia.java
+++ src/asistencias/Asistencia.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package asistencias;
 
-/**
- *
- * @author bryan
- */
+package asistencias;
+import participantes,Participante;
+import entrenamientos.Entrenamiento;
+
 public class Asistencia {
+    private Participante participante;
+    private Entrenamiento entrenamiento;
+    private boolean confirmar;
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public Entrenamiento getEntrenamiento() {
+        return entrenamiento;
+    }
+
+    public boolean isConfirmar() {
+        return confirmar;
+    }
+
+    public Asistencia(Participante participante, Entrenamiento entrenamiento, boolean confirmar) {
+        this.participante = participante;
+        this.entrenamiento = entrenamiento;
+        this.confirmar = confirmar;
+    }
+
+    @Override
+    public String toString() {
+        return "Asistencia{" + "participante=" 
+                + participante + ", entrenamiento="
+                + entrenamiento + ", confirmar=" 
+                + confirmar + '}';
+    }
     
+    
+   
 }
