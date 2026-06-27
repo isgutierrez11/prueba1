3bc7844732b3ffb881ab4cd7661ac941dbe8c027
Bryan <bryanlaraarguello@gmail.com>
2026-06-27 15:34:57 -0600

    Creacion de la clase responsable, con sus atributos metodos get y set y constructor
    Clase de LENNOX

---
+++ src/responsables/Responsable.java
@@ -0,0 +1,23 @@
+package responsables;
+
+
+import participantes.Participante
+import java.time.LocalDate;
+
+public class Responsable extends Participante {
+private String especialidad;
+private int experiencia;
+
+public Responsable(String especialidad, int experiencia,int identificacion, String nombrecompleto, LocalDate fechadenacimiento, int telefono) {
+super(identificacion, nombrecompleto, fechadenacimiento, telefono);
+this.especialidad = especialidad;
+this.experiencia = experiencia;
+  }
+    
+public void Infromacion(){
+        System.out.println("especialidad: " + especialidad);
+        System.out.println("Experiencia: " + experiencia);
+
+}
+    
+}
